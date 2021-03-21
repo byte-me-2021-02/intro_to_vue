f172ce7d7b82dfa4628662bca5f36f55f19213cb
brian <riceb53@gmail.com>
2021-03-21 10:26:50 -0400

    add v-if

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ var app = new Vue({
   el: "#app",
   data: {    
     message: "Hello from JavaScript!",
-    name: "briann"
+    name: "briann",
+    showSecret: false
   },
   methods: {
     changeMessage: function() {
       console.log('changing messaage...')
       this.message = "oooooooo";      
+    },
+    toggleSecret: function() {
+      console.log('toggling secret...');
+      this.showSecret = !this.showSecret;
+      // if (this.showSecret === true) {
+      //   this.showSecret = false;
+      // } else {
+      //   this.showSecret = true;
+      // }
     }
   }
 });
